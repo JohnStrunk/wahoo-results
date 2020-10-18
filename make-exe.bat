45221b8863570c561245080d2ab1b38808f78967
John Strunk <john.strunk@gmail.com>
2020-10-17 21:07:16 -0400

    Adjust exe generator for changed name
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- make-exe.bat
+++ make-exe.bat
@@ -6,7 +6,7 @@ call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 pip install --upgrade -r requirements.txt
 
-pyinstaller --onefile --noconsole --distpath=. --workpath=build --version-file=wahoo-results.version wahoo-results.py
+pyinstaller --onefile --noconsole --distpath=. --workpath=build --version-file=wahoo-results.version --name wahoo-results wahoo_results.py
 
 del wahoo-results.spec
 rmdir /q/s build
