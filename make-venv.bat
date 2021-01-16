63bccd0a2057eed2cc968e6699ac13f29b918404
John Strunk <john.strunk@gmail.com>
2021-01-16 12:20:40 -0500

    Install wheel when creating venv
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- make-venv.bat
+++ make-venv.bat
@@ -3,7 +3,7 @@ python -m venv venv || goto :error
 call venv\Scripts\activate.bat || goto :error
 
 ::: Upgrade pip to latest
-python -m pip install --upgrade pip || goto :error
+python -m pip install --upgrade pip wheel || goto :error
 
 ::: Install dependencies
 pip install --upgrade -r requirements.txt || goto :error
