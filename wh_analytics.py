bee0f3abd4d40ecefcaecac3d1301f507982fefa
John Strunk <john.strunk@gmail.com>
2023-06-11 10:12:03 -0400

    Broaden http exceptions handled when contacting ipinfo
    WAHOO-RESULTS-V
    
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- wh_analytics.py
+++ wh_analytics.py
@@ -24,6 +24,7 @@ from pprint import pprint
 from typing import Any, Dict, Optional, Tuple
 
 import ipinfo  # type: ignore
+import ipinfo.exceptions  # type: ignore
 import requests
 import sentry_sdk
 from segment import analytics  # type: ignore
@@ -191,7 +192,13 @@ def _setup_context(screen_size: Tuple[int, int]) -> Dict[str, Any]:
             "longitude": ipdetails.longitude,
         }
         context["timezone"] = ipdetails.timezone
-    except requests.ConnectTimeout:
+    except requests.HTTPError:  # General HTTP error
+        pass
+    except requests.JSONDecodeError:  # Invalid JSON returned
+        pass
+    except requests.Timeout:  # ConnectTimeout or ReadTimeout
+        pass
+    except ipinfo.exceptions.RequestQuotaExceededError:  # Over quota limit
         pass
 
     print(context)
